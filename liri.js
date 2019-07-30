6ec2f46282a4dfd7da7c12f39702e6acd7d25ef7
Eric Jobin <ejobin@gmail.com>
2019-07-30 17:37:56 -0400

    Removed testing code for Concert-This

--- liri.js
+++ liri.js
@@ -3,49 +3,47 @@ var axios = require("axios");
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 var inquirer = require("inquirer");
+var moment = require("moment");
 
 
 // Commenting Out Menu selection for development purposes
 
-// inquirer
-//     .prompt([
-//         {
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: ["Concert-This - Find upcoming concert events for an artist!", 
-//             "Spotify-This-Song - Find information about a song", 
-//             "Movie-This - Find information about a movie", 
-//             "Do-What-It-Says - This will do a search from the content of a text file", 
-//             "Quit"],
-//         name: "programFunction"
-//         },
-//     ])
-//     .then(function(inquirerResponse) {
+inquirer
+    .prompt([
+        {
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Concert-This - Find upcoming concert events for an artist!", 
+            "Spotify-This-Song - Find information about a song", 
+            "Movie-This - Find information about a movie", 
+            "Do-What-It-Says - This will do a search from the content of a text file", 
+            "Quit"],
+        name: "programFunction"
+        },
+    ])
+    .then(function(inquirerResponse) {
         
-//         if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
-//         concertThis();
-//         }
-//         else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
-//             console.log("Spotify-This-Song");
-//         }
-//         else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
-//             console.log("Movie-This");
-//         }
-//         else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
-//             console.log("Do-What-It-Says");
-//         }
-//         else if (inquirerResponse.programFunction == "Quit") {
-//             console.log("Goodbye");
-//         }
-//         else {
-//         console.log("Something has gone very wrong");
-//         }
-//     });
+        if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
+        concertThis();
+        }
+        else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
+            console.log("Spotify-This-Song");
+        }
+        else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
+            console.log("Movie-This");
+        }
+        else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
+            console.log("Do-What-It-Says");
+        }
+        else if (inquirerResponse.programFunction == "Quit") {
+            console.log("Goodbye");
+        }
+        else {
+        console.log("Something has gone very wrong");
+        }
+    });
 
 // ------------------------------------------------------------------------------------------------
-
-concertThis()
-
 
 concertThis = function(){
     inquirer
@@ -56,39 +54,25 @@ concertThis = function(){
             name: "artist"
         },])
     .then(function(inquirerResponse) {
-        var artist = escape(inquirerResponse.artist);
-        var queryUrl= (`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
+        var artist = inquirerResponse.artist;
+        var artistURL = escape(artist);
+        var queryUrl= (`https://rest.bandsintown.com/artists/${artistURL}/events?app_id=codingbootcamp`)
         console.log(queryUrl);
 
         axios.get(queryUrl).then(
             function(response) {
-                // console.log(response.data)
                 var concerts = response.data;
-                console.log(concerts);
-
-
-
-
-
-
+                listConcert(concerts, artist);
         });
     })
 
 }
-
-
-//    * `concert-this`
-//      * Name of the venue
-//      * Venue location
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-
-
-
-
-
-
-
+listConcert =function(concerts, artist){
+    for (var x=0;x<concerts.length;x++){
+        var formatTime = moment(concerts[x].datetime).format('MMMM Do YYYY');
+        console.log(`\n${artist} will be playing at ${concerts[x].venue.name}\nOn ${formatTime} in ${concerts[x].venue.city}\n`);
+    }
+}
 
 //-------------------------------------------------------------------------------
 
