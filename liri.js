b1adb8e7de831316e11c207d19bdf3e434dc6a70
Eric Jobin <ejobin@gmail.com>
2019-07-30 16:47:48 -0400

    Working on Concert This Function

--- liri.js
+++ liri.js
@@ -3,43 +3,49 @@ var axios = require("axios");
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 var inquirer = require("inquirer");
-// var bandsintown = require('bandsintown')('codingbootcamp');
 
-inquirer
-    .prompt([
-        {
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Concert-This - Find upcoming concert events for an artist!", 
-            "Spotify-This-Song - Find information about a song", 
-            "Movie-This - Find information about a movie", 
-            "Do-What-It-Says - This will do a search from the content of a text file", 
-            "Quit"],
-        name: "programFunction"
-        },
-    ])
-    .then(function(inquirerResponse) {
+
+// Commenting Out Menu selection for development purposes
+
+// inquirer
+//     .prompt([
+//         {
+//         type: "list",
+//         message: "What would you like to do?",
+//         choices: ["Concert-This - Find upcoming concert events for an artist!", 
+//             "Spotify-This-Song - Find information about a song", 
+//             "Movie-This - Find information about a movie", 
+//             "Do-What-It-Says - This will do a search from the content of a text file", 
+//             "Quit"],
+//         name: "programFunction"
+//         },
+//     ])
+//     .then(function(inquirerResponse) {
         
-        if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
-        // console.log("Concert-This");
-        concertThis();
-        }
-        else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
-            console.log("Spotify-This-Song");
-        }
-        else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
-            console.log("Movie-This");
-        }
-        else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
-            console.log("Do-What-It-Says");
-        }
-        else if (inquirerResponse.programFunction == "Quit") {
-            console.log("Goodbye");
-        }
-        else {
-        console.log("Something has gone very wrong");
-        }
-    });
+//         if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
+//         concertThis();
+//         }
+//         else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
+//             console.log("Spotify-This-Song");
+//         }
+//         else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
+//             console.log("Movie-This");
+//         }
+//         else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
+//             console.log("Do-What-It-Says");
+//         }
+//         else if (inquirerResponse.programFunction == "Quit") {
+//             console.log("Goodbye");
+//         }
+//         else {
+//         console.log("Something has gone very wrong");
+//         }
+//     });
+
+// ------------------------------------------------------------------------------------------------
+
+concertThis()
+
 
 concertThis = function(){
     inquirer
@@ -56,31 +62,26 @@ concertThis = function(){
 
         axios.get(queryUrl).then(
             function(response) {
-                console.log(response.data)
-                //   console.log(response.data.Year)
+                // console.log(response.data)
+                var concerts = response.data;
+                console.log(concerts);
+
+
+
+
+
+
         });
-
-        // bandsintown
-        // .getArtistEventList('Skrillex')
-        // .then(function(events) {
-        //     // return array of events
-        //     console.log(events)
-        // });
-
-
-
     })
 
 }
 
 
 //    * `concert-this`
-// 1. `node liri.js concert-this <artist/band name here>`
-//    * This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
 //      * Name of the venue
 //      * Venue location
 //      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-//url: (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) 
+
 
 
 
