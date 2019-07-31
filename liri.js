5c7a631aeddf0a47ea66513e3ef00e52ac655068
Eric Jobin <ejobin@gmail.com>
2019-07-31 12:49:07 -0400

    Coded logic for Do-What-It-Says option of reading in text file operation

--- liri.js
+++ liri.js
@@ -5,45 +5,48 @@ var keys = require("./keys.js");
 var inquirer = require("inquirer");
 var moment = require("moment");
 
-
 // Commenting Out Menu selection for development purposes
 
-inquirer
-    .prompt([
-        {
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Concert-This - Find upcoming concert events for an artist!", 
-            "Spotify-This-Song - Find information about a song", 
-            "Movie-This - Find information about a movie", 
-            "Do-What-It-Says - This will do a search from the content of a text file", 
-            "Quit"],
-        name: "programFunction"
-        },
-    ])
-    .then(function(inquirerResponse) {
+// inquirer
+//     .prompt([
+//         {
+//         type: "list",
+//         message: "What would you like to do?",
+//         choices: ["Concert-This - Find upcoming concert events for an artist!", 
+//             "Spotify-This-Song - Find information about a song", 
+//             "Movie-This - Find information about a movie", 
+//             "Do-What-It-Says - This will do a search from the content of a text file", 
+//             "Quit"],
+//         name: "programFunction"
+//         },
+//     ])
+//     .then(function(inquirerResponse) {
         
-        if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
-        concertThis();
-        }
-        else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
-            console.log("Spotify-This-Song");
-        }
-        else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
-            console.log("Movie-This");
-        }
-        else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
-            console.log("Do-What-It-Says");
-        }
-        else if (inquirerResponse.programFunction == "Quit") {
-            console.log("Goodbye");
-        }
-        else {
-        console.log("Something has gone very wrong");
-        }
-    });
+//         if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
+//         concertThis();
+//         }
+//         else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
+//             // console.log("Spotify-This-Song");
+//             spotThis();
+//         }
+//         else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
+//             // console.log("Movie-This");
+//             movieThis();
+//         }
+//         else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
+//             console.log("Do-What-It-Says");
+//             doWhat()
+            
+//         }
+//         else if (inquirerResponse.programFunction == "Quit") {
+//             console.log("Goodbye");
+//         }
+//         else {
+//         console.log("Something has gone very wrong");
+//         }
+//     });
 
-// ------------------------------------------------------------------------------------------------
+// -----------------------------------Concert-This-------------------------------------------------------------
 
 concertThis = function(){
     inquirer
@@ -57,7 +60,6 @@ concertThis = function(){
         var artist = inquirerResponse.artist;
         var artistURL = escape(artist);
         var queryUrl= (`https://rest.bandsintown.com/artists/${artistURL}/events?app_id=codingbootcamp`)
-        console.log(queryUrl);
 
         axios.get(queryUrl).then(
             function(response) {
@@ -74,7 +76,30 @@ listConcert =function(concerts, artist){
     }
 }
 
-//-------------------------------------------------------------------------------
+//-------------------------------Spotify-This-Song------------------------------------------------
+
+spotThis = function(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What song are you looking for?",
+            name: "song"
+        },])
+    .then(function(inquirerResponse) {
+        // var song = inquirerResponse.song;
+        // var songURL = escape(song);
+        // var queryUrl= (`https://rest.bandsintown.com/artists/${songURL}/events?app_id=codingbootcamp`)
+        // console.log(queryUrl);
+
+        // axios.get(queryUrl).then(
+        //     function(response) {
+        //         var concerts = response.data;
+        //         listConcert(concerts, artist);
+        // });
+    })
+
+}
 
 //    * `spotify-this-song`
 
@@ -93,7 +118,31 @@ listConcert =function(concerts, artist){
 //    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
 
 
-//---------------------------------------------------------------------------------
+//------------------------------Movie-This---------------------------------------------------
+
+movieThis = function(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What movie are you looking for?",
+            name: "movie"
+        },])
+    .then(function(inquirerResponse) {
+        var movie = inquirerResponse.movie;
+        var movieURL = escape(movie);
+        var queryUrl= (`https://rest.bandsintown.com/artists/${movieURL}/events?app_id=codingbootcamp`)
+   
+        // axios.get(queryUrl).then(
+        //     function(response) {
+        //         var concerts = response.data;
+        //         listConcert(concerts, artist);
+        // });
+    })
+
+}
+
+
 
 //    * `movie-this`
 
@@ -117,8 +166,39 @@ listConcert =function(concerts, artist){
 //    * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
 
 
-//------------------------------------------------------------------------------------------------------
+//----------------------------------------do-what-it-says--------------------------------------------
 
+
+doWhat = function(){
+    var fs = require("fs");
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // var method, term;
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        for (x=0;x<dataArr.length;x++){
+            console.log(dataArr[x].trim())
+        }
+
+        // if statements then calling stuff
+
+        if (dataArr[0].toLowerCase() == "concert-this") {
+            console.log("Concert-This "+ dataArr[1]);
+        }
+        else if (dataArr[0].toLowerCase() == "spotify-this-song") {
+            console.log("Spotify-This-Song "+ dataArr[1]);
+            
+        }
+        else if (dataArr[0].toLowerCase() == "movie-this") {
+            console.log("Moive-This "+ dataArr[1]);
+        }
+        else{console.log("Error: Not a valid method");}
+    });
+}
+
+//Format: spotify-this-song,"I Want it That Way"
 //    * `do-what-it-says`
 
 // 4. `node liri.js do-what-it-says`
@@ -133,9 +213,6 @@ listConcert =function(concerts, artist){
 // * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
 // * Make sure you append each command you run to the `log.txt` file. 
 // * Do not overwrite your file each time you run a command.
-
-
-
 
 
 
