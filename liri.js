ec08eee7ba5ed3d390006fb7bcffd18ea33d1ba9
Eric Jobin <ejobin@gmail.com>
2019-08-01 00:43:42 -0400

    Coded in functionality for do this. Restored menu functionality

--- liri.js
+++ liri.js
@@ -7,45 +7,45 @@ var moment = require("moment");
 
 
 // Commenting Out Menu selection for development purposes
-
-// inquirer
-//     .prompt([
-//         {
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: ["Concert-This - Find upcoming concert events for an artist!", 
-//             "Spotify-This-Song - Find information about a song", 
-//             "Movie-This - Find information about a movie", 
-//             "Do-What-It-Says - This will do a search from the content of a text file", 
-//             "Quit"],
-//         name: "programFunction"
-//         },
-//     ])
-//     .then(function(inquirerResponse) {
+//----------------------------------------------Menu Selector----------------------------------------------
+inquirer
+    .prompt([
+        {
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Concert-This - Find upcoming concert events for an artist!", 
+            "Spotify-This-Song - Find information about a song", 
+            "Movie-This - Find information about a movie", 
+            "Do-What-It-Says - This will do a search from the content of a text file", 
+            "Quit"],
+        name: "programFunction"
+        },
+    ])
+    .then(function(inquirerResponse) {
         
-//         if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
-//         concertThis();
-//         }
-//         else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
-//             // console.log("Spotify-This-Song");
-//             spotThis();
-//         }
-//         else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
-//             // console.log("Movie-This");
-//             movieThis();
-//         }
-//         else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
-//             console.log("Do-What-It-Says");
-//             doWhat()
+        if (inquirerResponse.programFunction == "Concert-This - Find upcoming concert events for an artist!") {
+        concertThis();
+        }
+        else if (inquirerResponse.programFunction == "Spotify-This-Song - Find information about a song") {
+            // console.log("Spotify-This-Song");
+            spotThis();
+        }
+        else if (inquirerResponse.programFunction == "Movie-This - Find information about a movie") {
+            // console.log("Movie-This");
+            movieThis();
+        }
+        else if (inquirerResponse.programFunction == "Do-What-It-Says - This will do a search from the content of a text file") {
+            console.log("Do-What-It-Says");
+            doWhat()
             
-//         }
-//         else if (inquirerResponse.programFunction == "Quit") {
-//             console.log("Goodbye");
-//         }
-//         else {
-//         console.log("Something has gone very wrong");
-//         }
-//     });
+        }
+        else if (inquirerResponse.programFunction == "Quit") {
+            console.log("Goodbye");
+        }
+        else {
+        console.log("Something has gone very wrong");
+        }
+    });
 
 // -----------------------------------Concert-This-------------------------------------------------------------
 
@@ -61,6 +61,7 @@ concertThis = function(){
         var artist = inquirerResponse.artist;
         var artistURL = escape(artist);
         var queryUrl= (`https://rest.bandsintown.com/artists/${artistURL}/events?app_id=codingbootcamp`)
+        console.log(queryUrl);
 
         axios.get(queryUrl).then(
             function(response) {
@@ -90,8 +91,7 @@ spotThis = function(){
         },])
     .then(function(inquirerResponse) {
         var song = inquirerResponse.song;
-
-        // console.log(keys)
+        if(song==""){song="Ace of Spades"}
  
         var spotify = new Spotify({
             id: keys.spotify.id,
@@ -112,27 +112,6 @@ spotThis = function(){
         });
     })
 }
-
-spotThis()
-
-//* ************  End of Test Code, comments below  ************************ */
-
-//    * `spotify-this-song`
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-//    * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-//    * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-//    * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-//    * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-//    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
 
 //------------------------------Movie-This---------------------------------------------------
 
@@ -171,46 +150,71 @@ movieThis = function(){
 
 //----------------------------------------do-what-it-says--------------------------------------------
 
-
 doWhat = function(){
     var fs = require("fs");
-
     fs.readFile("random.txt", "utf8", function(error, data) {
-        // var method, term;
         if (error) {
             return console.log(error);
         }
         var dataArr = data.split(",");
-        for (x=0;x<dataArr.length;x++){
-            console.log(dataArr[x].trim())
-        }
-
-        // if statements then calling stuff
-        //searchTerm=escape(searchTerm.trim())
 
         if (dataArr[0].toLowerCase() == "concert-this") {
             console.log("Concert-This "+ dataArr[1]);
+            var artist = dataArr[1];
+            var artistURL = escape(artist);
+            var queryUrl= (`https://rest.bandsintown.com/artists/${artistURL}/events?app_id=codingbootcamp`)
+
+            axios.get(queryUrl).then(
+                function(response) {
+                    var concerts = response.data;
+                    listConcert(concerts, artist);
+            });
         }
         else if (dataArr[0].toLowerCase() == "spotify-this-song") {
-            console.log("Spotify-This-Song "+ dataArr[1]);
+            var song = dataArr[1];
+     
+            var spotify = new Spotify({
+                id: keys.spotify.id,
+                secret: keys.spotify.secret
+            });
             
+            spotify.search({ type: 'track', query: song }, function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log(`
+                    Artist: ${data.tracks.items[0].album.artists[0].name}\n
+                    Song: ${data.tracks.items[0].name}\n
+                    Preview Link: ${data.tracks.items[0].preview_url}\n
+                    Album: ${data.tracks.items[0].album.name}\n
+                `)
+            });
         }
         else if (dataArr[0].toLowerCase() == "movie-this") {
-            console.log("Moive-This "+ dataArr[1]);
+            var movie = dataArr[1];
+            var movieURL = escape(movie);
+            var queryUrl= (`http://www.omdbapi.com/?t=${movieURL}&y=&plot=short&apikey=trilogy`)
+            console.log(queryUrl)
+    
+            axios.get(queryUrl).then(
+                function(response) {
+                    console.log(`
+                        Title: ${response.data.Title}\n
+                        Year: ${response.data.Year}\n
+                        IMDB Rating: ${response.data.imdbRating}\n
+                        Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}\n
+                        Country: ${response.data.Country}\n
+                        Language: ${response.data.Language}\n
+                        Plot: ${response.data.Plot}\n
+                        Actors: ${response.data.Actors}
+                    `)
+                }
+            )
+
         }
         else{console.log("Error: Not a valid method");}
     });
 }
-
-//Format: spotify-this-song,"I Want it That Way"
-//    * `do-what-it-says`
-
-// 4. `node liri.js do-what-it-says`
-
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
 //-----------------------------------------------------------------------------------------------
 
